9759b81b1b42961166982c9d28defd96ac080387
AlexJ2598 <67809818+AlexJ2598@users.noreply.github.com>
2024-01-14 01:37:49 -0600

    Avance clase 3
    Calculadora sencilla en html más javascript más una función de recorrdio de radiobuttons byName

--- javascript/metodos.js
+++ javascript/metodos.js
@@ -1,9 +1,42 @@
+'use strict';
+
+function muestraOpcion(){
+    var opciones = document.getElementsByName('eleccion');
+    console.log(opciones);
+
+    for(let i = 0; i < opciones.length; i++){
+        if(opciones[i].checked){
+            alert(opciones[i].value);
+        }
+    }
+}
+
 function calculo(){
-    var no1 = document.getElementById('num1');
-    var no2 = document.getElementById('num2');
-    console.log(no1);
-    var resultado = parseInt(no1) + parseInt(no2);
-    //Mandamos de regreso.
-    //$('#resultado').text(resultado);
-    //alert('El resultado de tu suma es: ' + resultado);
+    var no1 = Number(document.getElementById('num1').value);
+    var no2 = Number(document.getElementById('num2').value);
+    var resultado;
+    var option = document.getElementById('operacion').value.toString();
+    //console.log(option); para verificar si ya habiamos obtenido las operaciones.
+    switch(option){
+        case 'suma':
+            resultado = no1 + no2;
+            document.getElementById('resultado').innerHTML = resultado;
+            break;
+        case 'resta':
+            resultado = no1 - no2;
+            document.getElementById('resultado').innerHTML = resultado;
+            break;
+        case 'multiplicacion':
+            resultado = no1 * no2;
+            document.getElementById('resultado').innerHTML = resultado;
+            break;
+        case 'division':
+            try{
+                resultado = no1/no2;
+                document.getElementById('resultado').innerHTML = resultado;
+            }catch{
+                alert('No puedes dividir entre 0');
+            }
+            break;
+    }
 }
